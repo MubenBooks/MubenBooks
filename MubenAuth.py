484c69d6bf3de245928de4ec746c24858b7888e5
Nicolana <juniverst@outlook.com>
2018-12-02 15:09:28 +0800

    init register function

--- MubenAuth.py
+++ MubenAuth.py
@@ -2,6 +2,7 @@
 # login 
 # logout
 # register
+import bcrypt
 import logging
 import tornado.web
 from BaseHandler import *
@@ -31,4 +32,27 @@ class AuthRegisterHandler(BaseHandler):
         self.render("create_user.html")
 
     async def post(self):
-        self.write("function is still developing...")
+        if await self.any_user_exists(self.get_argument("username")):
+            raise tornado.web.HTTPError(400, "用户已经存在")
+        hashed_password = await tornado.ioloop.IOLoop.current().run_in_executor(
+                None,
+                bcrypt.hashpw,
+                tornado.escape.utf8(self.get_argument("password")),
+                bcrypt.gensalt(),
+        )
+
+        user = await self.queryone(
+                "INSERT INTO users (username, password, post_email, register_email, phone_num) "
+                "VALUES (%s, %s, %s, %s, %s) RETURN id",
+                self.get_argument("username"),
+                tornado.escape.to_unicode(hashed_password),
+                self.get_argument("post_email"),
+                self.get_argument("register_email"),
+                self.get_argument("phone_num"),
+                )
+        self.set_secure_cookie('muben_user', str(user.id))
+        self.redirect(self.get_argument("next", "/"))
+
+
+
+
